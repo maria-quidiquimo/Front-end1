b2e133fd0789bd07cd4d3a3758d4e5dc0996568c
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-03-26 12:39:41 -0300

    fazendo exercício 6

--- Aula-05/Exercícios_aula5e6/Segundo exercício/src/scripts/script-aula6.js
+++ Aula-05/Exercícios_aula5e6/Segundo exercício/src/scripts/script-aula6.js
@@ -1,9 +1,41 @@
-const botaoCurtir = document.querySelector('.card-acao')
+//1 - Interação (Clique): Ao clicar no botão "Curtir", incremente o número no contador de curtidas.
+const btn = document.querySelector('#btn-curtir')
+const contadorUsuario = document.querySelector('#contador')
 
-botaoCurtir.addEventListener('click', (event) =>{
-    const clicado = event.target
+let totalCurtidas = 0
 
-    if(clicado.classList.contains('contador')){
-        console.log("Você clicou em um botão de Curtir!")
+btn.addEventListener('click', function() {
+    totalCurtidas++
+    contadorUsuario.textContent = totalCurtidas
+})
+
+//2 - Monitoramento (Input): Sempre que o usuário digitar no campo de texto, o parágrafo de "Preview" deve mostrar o texto em tempo real.
+const escritaUsuario = document.querySelector('#campo-texto')
+const previaUsuario = document.querySelector('#preview-texto')
+
+escritaUsuario.addEventListener('input', function() {
+    if(escritaUsuario.value === "") {
+        previaUsuario.textContent = 'Digitando: ...'
+    } else {
+        previaUsuario.textContent = `Digitando: ${escritaUsuario.value}`
     }
+})
+
+//3 - Sensores (Mouse): Ao entrar com o mouse na caixa de cor, mude-a para azul. Ao sair, ela deve voltar à cor original.
+const corCaixa = document.querySelector('#caixa-cor')
+corCaixa.addEventListener('mouseover', function() {
+    corCaixa.style.backgroundColor = '#6b8abd'
+})
+
+corCaixa.addEventListener('mouseout', function() {
+    corCaixa.style.backgroundColor = '#95a5a6'
+})
+
+//4 - Desafio Extra (Reset): Crie um botão (ou use uma tecla) que limpe o input e zera o contador ao mesmo tempo.
+const botaoReset = document.querySelector('#btn-reset')
+botaoReset.addEventListener('click', function() {
+    escritaUsuario.value = ''
+    previaUsuario.textContent = 'Digitando: ...'
+    totalCurtidas = 0
+    contadorUsuario.textContent = '0'
 })
