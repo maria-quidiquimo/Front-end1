ae1681ae39ce1a42bf770eb24b646d11d11782f3
maria-quidiquimo <maria.e.barreto12@aluno.senai.br>
2026-04-02 15:29:53 -0300

    fazendo a somativa tudinho

--- Somativa-Maria-Eduarda-Quidiquimo/src/scripts/script.js
+++ Somativa-Maria-Eduarda-Quidiquimo/src/scripts/script.js
@@ -12,3 +12,39 @@ if(horario < 12){
     alert(`Boa noite, Mestre Jedi!`)
     console.log("Depois das 18h")
 }
+
+const promo = document.querySelectorAll("#banner-promo");
+
+promo.forEach((promocao) =>{
+    promocao.addEventListener("mouseover", () =>{
+        promo.classList.add('destaque-dark')
+    })
+    promocao.addEventListener("mouseout", () =>{
+        promo.classList.remove('destaque-dark')
+    })
+})
+
+const qtdItem = document.querySelector("#qtd-item");
+const precoItem = document.querySelector("#resultado-preco")
+
+if(qtdItem && precoItem){
+    qtdItem.addEventListener("input", () => {
+        const precoUnitario = 85;
+        const resultadoPreco = Number(qtdItem.value) * precoUnitario;
+        precoItem.textContent = `R$ ${resultadoPreco.toFixed(2)}`
+    })
+}
+
+const botaoAdicionar = document.querySelector('#btn-adicionar');
+const nomeProduto = document.querySelector('#nome-produto');
+const listaCarrinho = document.querySelector('#lista-carrinho');
+
+botaoAdicionar.addEventListener("input", () => {
+    botaoAdicionar.innerHTML = `<article class="card-carrinho"><h3>🎮 Item: ${nomeProduto} </h3></article>`
+})
+
+const botaoLimpar = document.querySelector('#btn-limpar');
+
+botaoLimpar.addEventListener("click", () => {
+    botaoLimpar.innerHTML = ``
+})
